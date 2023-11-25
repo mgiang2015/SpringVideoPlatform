ba2a4652c23f06898887c0fda4dd11194b371e23
mgiang2015 <mgiang2015@gmail.com>
2023-11-26 00:47:22 +0800

    connect front-end to back-end

--- src/main/java/com/mgiang2015/SpringVideoPlatform/SpringVideoPlatformApplication.java
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/SpringVideoPlatformApplication.java
@@ -2,12 +2,25 @@ package com.mgiang2015.SpringVideoPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringVideoPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringVideoPlatformApplication.class, args);
+	}
+
+	// Enables CORS
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
--- src/main/js/src/App.jsx
+++ src/main/js/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    // simple GET request
+    fetch("http://localhost:8080/users")
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <>
