b899129a1647eb3d23920c421809dbeee734399b
mgiang2015 <mgiang2015@gmail.com>
2023-11-25 21:20:01 +0800

    Add setId back

--- src/main/java/com/mgiang2015/SpringVideoPlatform/model/User.java
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/model/User.java
@@ -29,6 +29,10 @@ public class User {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Date getCreatedDate() {
         return createdDate;
     }
--- src/main/java/com/mgiang2015/SpringVideoPlatform/model/Video.java
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/model/Video.java
@@ -27,6 +27,9 @@ public class Video {
     public Long getId() {
         return id;
     }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public Date getCreatedDate() {
         return createdDate;
     }
