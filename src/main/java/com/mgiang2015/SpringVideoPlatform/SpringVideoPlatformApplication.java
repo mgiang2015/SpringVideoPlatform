0f233a4c5079f76f751da5f91a6408a3553298af
mgiang2015 <mgiang2015@gmail.com>
2023-11-25 16:31:49 +0800

    First commit

---
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/SpringVideoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mgiang2015.SpringVideoPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVideoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVideoPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgiang2015/SpringVideoPlatform/SpringVideoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgiang2015.SpringVideoPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVideoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
