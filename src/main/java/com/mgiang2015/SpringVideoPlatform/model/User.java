623bdc7ac06e46cdf0f4c1b8cb3b951bd5b3cfc3
mgiang2015 <mgiang2015@gmail.com>
2023-11-25 20:47:12 +0800

    Make auto-generated fields read-only

--- src/main/java/com/mgiang2015/SpringVideoPlatform/model/User.java
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/model/User.java
@@ -28,21 +28,15 @@ public class User {
     public Long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
+
     public Date getCreatedDate() {
         return createdDate;
     }
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
+
     public Date getLastModifiedDate() {
         return lastModifiedDate;
     }
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
+
     public String getEmail() {
         return email;
     }
@@ -61,6 +55,4 @@ public class User {
     public void setCreator(boolean isCreator) {
         this.isCreator = isCreator;
     }
-
-    
 }
--- src/main/java/com/mgiang2015/SpringVideoPlatform/model/Video.java
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/model/Video.java
@@ -27,20 +27,11 @@ public class Video {
     public Long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
     public Date getCreatedDate() {
         return createdDate;
     }
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
     public Date getLastModifiedDate() {
         return lastModifiedDate;
-    }
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
     }
     // public User getCreator() {
     //     return creator;
