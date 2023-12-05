83319413e8ecb73c0da9722016a47ab5f8a013c1
mgiang2015 <mgiang2015@gmail.com>
2023-12-05 22:29:03 +0800

    Add styling and UI for video file upload

--- src/main/js/src/pages/Upload.jsx
+++ src/main/js/src/pages/Upload.jsx
@@ -1,18 +1,23 @@
-import { Box, Button, Input, TextField } from '@mui/material';
+import { Button, Container, Input, TextField, Typography, LinearProgress } from '@mui/material';
 import React, { useState } from 'react';
 import axios from 'axios'
 
 export default function Upload() {
     const [title, setTitle] = useState("");
     const [file, setFile] = useState(null);
+    const [uploadProgress, setUploadProgress] = React.useState(0);
 
-    const handleSubmit = () => {
-        console.log(title);
-        console.log(file);
+    const handleSubmit = (e) => {
+        e.preventDefault();
         let formData = new FormData();
         formData.append('title', title);
         formData.append('file', file);
-        axios.post('http://localhost:8080/videos/add', formData)
+        axios.post('http://localhost:8080/videos/add', formData, {
+            onUploadProgress: (progressEvent) => {
+                const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                setUploadProgress(percentCompleted);
+            }
+        })
         .then((response) => {
             console.log(response);
         })
@@ -22,12 +27,18 @@ export default function Upload() {
     }
 
     return (
-        <Box sx={{ m: 5 }}>
-            <TextField onChange={(e) => setTitle(e.target.value)} />
-            <Input type='file' onChange={(e) => setFile(e.target.files[0])} />
-            <Button onClick={handleSubmit}>
-                Upload Video
-            </Button>
-        </Box>
+        <Container maxWidth="xl" sx={{ mt: 5 }}>
+            <Typography gutterBottom variant='h4'>Upload video</Typography>
+            <form onSubmit={handleSubmit}>
+                <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <TextField required label="Video title" placeholder='Your title here' onChange={(e) => setTitle(e.target.value)} sx={{ m: 2 }} />
+                    <Input required type='file' onChange={(e) => setFile(e.target.files[0])} sx={{ m: 2 }} />
+                    <Button type='submit' sx={{ m: 2 }}>
+                        Upload
+                    </Button>
+                </Container>
+            </form>
+            <LinearProgress variant="determinate" value={uploadProgress} />
+        </Container>
     )
 }
