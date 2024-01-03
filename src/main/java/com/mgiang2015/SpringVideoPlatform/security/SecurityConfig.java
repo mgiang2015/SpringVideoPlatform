809af87edb84d9a562761e488f28fb43acc9bf8e
mgiang2015 <mgiang2015@gmail.com>
2024-01-03 13:23:49 +0800

    Remove unnecessary import

--- src/main/java/com/mgiang2015/SpringVideoPlatform/security/SecurityConfig.java
+++ src/main/java/com/mgiang2015/SpringVideoPlatform/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
