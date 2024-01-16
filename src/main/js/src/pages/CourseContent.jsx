092def3aee73f0057aa5985b1eb61ee7fa8b1b4b
mgiang2015 <mgiang2015@gmail.com>
2024-01-16 18:30:39 +0800

    Add list to course content

--- src/main/js/src/pages/CourseContent.jsx
+++ src/main/js/src/pages/CourseContent.jsx
@@ -1,3 +1,19 @@
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import InboxIcon from '@mui/icons-material/Inbox';
+
 export default function CourseContent() {
-    return <h1>Hii from content</h1>
+    return (
+        <Box>
+            <List>
+                <ListItem>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Chapter 1: The Beninging"/>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    )
 }
