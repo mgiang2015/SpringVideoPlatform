04cfc816e341816df42de57ea41e854683c4c8c5
mgiang2015 <mgiang2015@gmail.com>
2024-01-29 15:59:57 +0800

    Improve layout

--- src/main/js/src/App.jsx
+++ src/main/js/src/App.jsx
@@ -5,30 +5,17 @@ import './App.css'
 import { useCookies } from 'react-cookie'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Learning Management System</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Basic Learning Management System written using Java Springboot and React.
+        </p>
+        <p>
+          Uses MongoDB and MySQL databases.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/main/js/src/Layout.jsx
+++ src/main/js/src/Layout.jsx
@@ -7,9 +7,9 @@ export default function Layout({ children }) {
         <Box sx={{ width: "100vw", height: "100vh" }}>
             <AppBar position="relative" width="100%">
                 <Toolbar>
-                    <Button disableElevation variant='contained' sx={{ m: 1 }} href='/'>Home</Button>
-                    <Button disableElevation variant='contained' sx={{ m: 1 }} href='/browse'>Browse</Button>
-                    <Button disableElevation variant='contained' sx={{ m: 1 }} href='/courses/create'>Create Course</Button>
+                    <Button disableElevation variant='contained' sx={{ m: 1, textTransform: "none" }} href='/'>Home</Button>
+                    <Button disableElevation variant='contained' sx={{ m: 1, textTransform: "none" }} href='/browse'>Browse</Button>
+                    <Button disableElevation variant='contained' sx={{ m: 1, textTransform: "none" }} href='/courses/create'>Create Course</Button>
                 </Toolbar>
             </AppBar>
             <Box sx={{ height: "100%", width: "100%" }}>{ children }</Box> 
